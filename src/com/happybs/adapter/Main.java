afd1b87ff600c982ff266dd0b59dc5fd52b1e3d6
happy-boksil <82457053+happy-boksil@users.noreply.github.com>
2022-03-23 00:30:39 +0900

    Delete Main.java

--- src/com/happybs/adapter/Main.java
+++
@@ -1,9 +0,0 @@
-package com.happybs.adapter;
-
-public class Main {
-    public static void main(String[] args) {
-        Print p = new PrintBanner("Hello");
-        p.printWeak();
-        p.printStrong();
-    }
-}
