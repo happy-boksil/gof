3118d4bd1dd5679052f75ec26aa0a15ce78af49a
happyboksil2@gmail.com <happyboksil2@gmail.com>
2022-04-10 17:28:20 +0900

    visitor

---
+++ src/com/happybs/visitor/FileTreatmentException.java
@@ -0,0 +1,10 @@
+package com.happybs.visitor;
+
+public class FileTreatmentException extends  RuntimeException {
+    public FileTreatmentException() {
+
+    }
+    public FileTreatmentException(String msg) {
+        super(msg);
+    }
+}
