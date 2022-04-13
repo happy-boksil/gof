b6272cb8eb12568aece9119217e41757e9b75a3c
happyboksil2@gmail.com <happyboksil2@gmail.com>
2022-04-13 23:54:15 +0900

    mediator

---
+++ src/com/happybs/meditator/ColleagueTextField.java
@@ -0,0 +1,22 @@
+package com.happybs.meditator;
+
+import java.awt.*;
+import java.awt.event.TextEvent;
+import java.awt.event.TextListener;
+
+public class ColleagueTextField extends TextField implements TextListener, Colleague{
+    @Override
+    public void setMediator(Mediator mediator) {
+
+    }
+
+    @Override
+    public void setColleagueEnabled(boolean enabled) {
+
+    }
+
+    @Override
+    public void textValueChanged(TextEvent e) {
+        
+    }
+}
---
+++ src/com/happybs/meditator/Mediator.java
@@ -0,0 +1,6 @@
+package com.happybs.meditator;
+
+public interface Mediator {
+    public abstract void createColleagues();
+    public abstract void colleagueChanged();
+}
