1e8a529715d73409f79c1acae7d0c372c20e180c
happyboksil2@gmail.com <happyboksil2@gmail.com>
2022-04-14 23:56:34 +0900

    mediator2

---
+++ src/com/happybs/meditator/LoginFrame.java
@@ -0,0 +1,41 @@
+package com.happybs.meditator;
+
+import java.awt.*;
+
+public class LoginFrame extends Frame {
+    private ColleagueCheckbox checkGuest;
+    private ColleagueCheckbox checkLogin;
+    private ColleagueTextField textUser;
+    private ColleagueTextField textPass;
+    private ColleagueButton buttonOk;
+    private ColleagueButton buttonCancel;
+
+    public LoginFrame(String title) {
+        super(title);
+        setBackground(Color.lightGray);
+        setLayout(new GridLayout(4,2));
+        createColleagues();
+        add(checkGuest);
+        add(checkLogin);
+        add(new Label("Username:"));
+        add(textUser);
+        add(new Label("Password:"));
+        add(textPass);
+        add(buttonCancel);
+        colleagueChanged();
+        pack();
+        show();
+    }
+
+    public void createColleagues() {
+        CheckboxGroup g = new CheckboxGroup();
+        checkGuest = new ColleagueCheckbox("Guest", g, true);
+        checkLogin = new ColleagueCheckbox("Login", g, false);
+        textUser = new ColleagueTextField("", 10);
+        textPass = new ColleagueTextField("", 10);
+        textPass.setEchoChar('*');
+        buttonOk = new ColleagueButton("OK");
+        buttonCancel = new ColleagueButton("Cancel");
+        
+    }
+}
